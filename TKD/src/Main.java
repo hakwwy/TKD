71594210654bee0d2a9070d6c12a8f5c2ffb119a
vandegor <Adamos500@gmail.com>
2014-11-21 13:32:31 +0100

    3rd commit

--- TKD/src/Main.java
+++ TKD/src/Main.java
@@ -1,10 +1,12 @@
 
-public class Main {
+public class Main 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		
 		System.out.println("Hello World!");
+		
 	}
 
 }
---
+++ TKD/src/Notation.java
@@ -0,0 +1,27 @@
+
+public class Notation 
+{
+	//polishNotation - PN
+	//reversePolishNotation - RPN
+	public static final int EXPRESSION_TYPE_NORMAL = 0;
+	public static final int EXPRESSION_TYPE_PN = 1;
+	public static final int EXPRESSION_TYPE_RPN = 2;
+	Notation()
+	{
+		
+	}
+	public static String toRPN(String expressionNormal,int expressionType)
+	{
+		return "";
+	}
+	
+	public static String toNormal(String expression,int expressionType)
+	{
+		return "";
+	}
+	
+	public static String toPN(String expression,int expressionType)
+	{
+		return "";
+	}
+}
